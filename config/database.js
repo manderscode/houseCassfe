5fd6b2ce132dd1024ef3cf376ffb0d056b4f9642
Resilient Coders <resilientcoders@Kerlins-MBP.fios-router.home>
2022-06-14 11:33:38 -0400

    updated the dotenv files and changed db and added gitIgnore file

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 module.exports = {
-  url:"mongodb+srv://mawong2:eVUa3qBDYCkoNjyi@cluster0.3xblz.mongodb.net/cassCafe?retryWrites=true&w=majority",
+  url:process.env.MONGODBURL,
   dbName: "cassCafe",
 };
